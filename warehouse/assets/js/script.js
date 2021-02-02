2a38a370632777e3f12568f7c09b27f23e0e2bbd
TommyRizzo <tommy.rizzo2002@gmail.com>
2021-02-02 17:42:49 +0100

    update-02-02-2020-17-42

--- warehouse/assets/js/script.js
+++ warehouse/assets/js/script.js
@@ -18,4 +18,23 @@
         $("#submit-damagedOrderChoice").css("display", "inline");
     });
 
+    $('#wrongOrderChoice #money').click(() => {
+        $('#wrongOrderChoice #money').css("background-color", "#3F3D56");
+        $('#wrongOrderChoice #money').css("color", "#FFFFFF");
+        $('#wrongOrderChoice #product').css("background-color", "#FFFFFF");
+        $('#wrongOrderChoice #product').css("color", "#8B8B8B");
+        $("#product-check").css("display", "none");
+        $("#money-check").css("display", "inline");
+        $("#submit-wrongOrderChoice").css("display", "inline");
+    });
+    $('#wrongOrderChoice #product').click(() => {
+        $('#wrongOrderChoice #product').css("background-color", "#3F3D56");
+        $('#wrongOrderChoice #product').css("color", "#FFFFFF");
+        $('#wrongOrderChoice #money').css("background-color", "#FFFFFF");
+        $('#wrongOrderChoice #money').css("color", "#8B8B8B");
+        $("#money-check").css("display", "none");
+        $("#product-check").css("display", "inline");
+        $("#submit-wrongOrderChoice").css("display", "inline");
+    });
+
 })();
